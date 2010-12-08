861353f66b746d9c4958a16138af28a75c2a388a
jabavin <jabavin@bfcd5bd5-0d9a-a3f9-f602-74cfab28e712>
2010-12-08 01:29:43 +0000

    Version 1.5.2 uploaded to Market

--- photogeolocator/src/edu/uoc/mperezma/Version.java
+++ photogeolocator/src/edu/uoc/mperezma/Version.java
@@ -29,7 +29,7 @@ public class Version extends Activity {
         mEditor.setText("" +
                 "Photo Geolocator" +
                 "\n" +
-                getText(R.string.version) + " 1.5.1 (Build 201011212314)" +
+                getText(R.string.version) + " 1.5.2 (Build 201012080203)" +
                 "\n" +
                 "Free GNU license" +
                 "\n" +
